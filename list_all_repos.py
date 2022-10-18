5b2bc894601ca91649f512b50f57c3d09c18abef
Chang Jo Kim <wowchangjo@gmail.com>
2022-10-18 16:28:13 +0900

    Import exception

--- list_all_repos.py
+++ list_all_repos.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
-
-import requests
+try:
+    import requests
+except ImportError:
+    raise ImportError("Please install requests module (pip install requests)")
 import argparse
 
 
-parser = argparse.ArgumentParser(description="List all repositories and tags in the docker registry")
-parser.add_argument('--url', type=str, default='https://10.0.0.161:5000', help='Docker registry URL')
+parser = argparse.ArgumentParser(
+    description="List all repositories and tags in the docker registry"
+)
+parser.add_argument(
+    "--url", type=str, default="https://10.0.0.161:5000", help="Docker registry URL"
+)
 args = parser.parse_args()
 
 
 class str_obj:
-    def __init__(self, value=''):
+    def __init__(self, value=""):
         self.value = value
 
     def __str__(self):
@@ -26,19 +32,19 @@ class str_obj:
 
 def show(data):
     result = []
-    equals = str_obj('=')
-    dashes = str_obj('-')
+    equals = str_obj("=")
+    dashes = str_obj("-")
 
     result.append(equals)
-    result.append('Registry URL: {}'.format(args.url))
-    result.append('{}:{}'.format('Repository', 'Tags'))
+    result.append("Registry URL: {}".format(args.url))
+    result.append("{}:{}".format("Repository", "Tags"))
     result.append(equals)
     repos = sorted(data.keys())
     for repo in repos:
         tags = sorted(data[repo])
         first_tag = True
         for tag in tags:
-            result.append('{}:{}'.format(repo, tag))
+            result.append("{}:{}".format(repo, tag))
         result.append(dashes)
     result.append(equals)
 
@@ -47,10 +53,10 @@ def show(data):
         if len(r) > max_len:
             max_len = len(r)
 
-    equals.value = max_len * '='
-    dashes.value = max_len * '-'
+    equals.value = max_len * "="
+    dashes.value = max_len * "-"
 
-    output = '\n'.join(map(str, result))
+    output = "\n".join(map(str, result))
     print(output)
 
 
@@ -59,19 +65,19 @@ def list_all():
     requests.urllib3.disable_warnings()
     data = {}
 
-    url = args.url + '/v2/_catalog'
+    url = args.url + "/v2/_catalog"
     response = requests.get(url, verify=False)
-    repos = response.json()['repositories']
+    repos = response.json()["repositories"]
     for repo in repos:
-        url = args.url + '/v2/{}/tags/list'.format(repo)
+        url = args.url + "/v2/{}/tags/list".format(repo)
         response = requests.get(url, verify=False)
         response_json = response.json()
-        if 'errors' not in response_json:
-            tags = response_json['tags']
+        if "errors" not in response_json:
+            tags = response_json["tags"]
             data[repo] = tags
 
     show(data)
 
-if __name__ == '__main__':
-    list_all()
 
+if __name__ == "__main__":
+    list_all()
